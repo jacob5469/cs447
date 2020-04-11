1d1eb801617d7461416e7460811411be484d9460
codeblockz <raemusick@gmail.com>
2020-04-11 12:42:20 -0400

    Ryan file changes

--- api-starter/src/index.ts
+++ api-starter/src/index.ts
@@ -28,14 +28,20 @@ const router = express.Router();
 // A Soda service will probably be created to facilitate all the soda stuff
 
 const consumer = new soda.Consumer("data.baltimorecity.gov");
-
-consumer.query().limit(1000000000).withDataset("wsfq-mvij").where(soda.expr.eq("CrimeTime", "00:30:00"), soda.expr.eq("CrimeTime", "00:30:00"))
+consumer.query().limit(1000000000).withDataset("wsfq-mvij").where(soda.expr.gt("CrimeDate", "04/04/2020"),soda.expr.gt("CrimeTime", "00:00:00"))
     .getRows().on("success", function (rows: any) {
 
         console.log(rows.length);
-
+        return rows[0].CrimeDate
     });
+/*
+ consumer.query().limit(1000000000).withDataset("wsfq-mvij").where(soda.expr.gt("CrimeDate", latestData.date),soda.expr.gt("CrimeTime", latestData.time))
+    .getRows().on("success", function (rows: any) {
 
+        console.log(rows.length);
+        return rows[0].CrimeDate
+    });
+*/
 app.use(router);
 app.use(bodyParser.json());
 
