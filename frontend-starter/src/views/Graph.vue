9e86c739a3f93be0c0a07de48db6c4166bedc01b
MaoLi <lm295722767@gmail.com>
2020-05-01 00:36:15 -0400

    add distribution of weekday graph

--- frontend-starter/src/views/Graph.vue
+++ frontend-starter/src/views/Graph.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
-    <GChart type="BarChart" @ready="onChartReady" />
-    <GChart
-      type="ColumnChart"
-      :data="chartData"
-      :options="chartOptions"
-      style="width: 900px; height: 500px;"
-    />
-    <GChart
-      type="BubbleChart"
-      :data="chartData2"
-      :options="chartOptions2"
-      style="width: 900px; height: 500px;"
-    />
+    <GChart type="BarChart" @ready="example" style="width: 900px; height: 500px;" />
   </div>
 </template>
 
@@ -26,47 +14,76 @@ import { Component, Vue } from "vue-property-decorator";
   }
 })
 export default class Graph extends Vue {
-  chartData = [
-    ["ID", "Assault", "Knife", "Kidnapping"],
-    ["", 80, 167, 120],
-    ["", 79, 136, 130],
-    ["", 78, 184, 50],
-    ["", 72, 278, 230],
-    ["", 81, 200, 210],
-    ["", 72, 170, 100],
-    ["", 68, 477, 80]
-  ];
-  chartOptions = {
-    colorAxis: { colors: ["yellow", "red"] },
-    title: "Type of Crimes"
-  };
-  chartData2 = [
-    ["ID", "WoodBerry", "Downtown", "Inner Harbor"],
-    ["", 80, 167, 120],
-    ["", 79, 136, 130],
-    ["", 78, 184, 50],
-    ["", 72, 278, 230],
-    ["", 81, 200, 210],
-    ["", 72, 170, 100],
-    ["", 68, 477, 80]
-  ];
-  chartOptions2 = {
-    title: "Area distribution",
-    colorAxis: { colors: ["yellow", "red"] }
-  };
-
-  onChartReady(chart, google) {
-    const query = new google.visualization.Query(
-      "https://docs.google.com/spreadsheets/d/1vDzr56Mxr9hv_eWmvwUW7ihPwnSNtkjJ32eDnFsK5Z8/edit?usp=sharing"
-    );
-    query.send(response => {
-      const options = {
-        title: "Age Distribution of Crime",
-        colors: ["blue"]
-      };
-      const data = response.getDataTable();
-      chart.draw(data, options);
-    });
+  async example(chart, google) {
+      const data = await (
+        await fetch("http://localhost:3000/api/map/data", {
+          headers: { "Content-Type": "application/json" },
+          method: "post",
+        })
+    ).json();
+    // const data = [
+    //     {
+    //       "WEEKDAY(crimedate)": 2,
+    //       "COUNT(*)": 43059
+    //     },
+    //     {
+    //       "WEEKDAY(crimedate)": 6,
+    //       "COUNT(*)": 40315
+    //     },
+    //     {
+    //       "WEEKDAY(crimedate)": 1,
+    //       "COUNT(*)": 43438
+    //     },
+    //     {
+    //       "WEEKDAY(crimedate)": 4,
+    //       "COUNT(*)": 44807
+    //     },
+    //     {
+    //       "WEEKDAY(crimedate)": 0,
+    //       "COUNT(*)": 44115
+    //     },
+    //     {
+    //       "WEEKDAY(crimedate)": 3,
+    //       "COUNT(*)": 42677
+    //     },
+    //     {
+    //       "WEEKDAY(crimedate)": 5,
+    //       "COUNT(*)": 41654
+    //     },
+    // ];
+    const map = new Map();
+    for (let i = 0; i < data.length; i++) {
+      const weekDay = data[i]['WEEKDAY(crimedate)'];
+      const numberOfCrime = data[i]['COUNT(*)'];
+      let day = "";
+      if (weekDay === 0) {
+        day = "Monday";
+      } else if (weekDay === 1) {
+        day = "Tuesday";
+      } else if (weekDay === 2) {
+        day = "Wednesday";
+      } else if (weekDay === 3) {
+        day = "Thursday";
+      } else if (weekDay === 4) {
+        day = "Friday";
+      } else if (weekDay === 5) {
+        day = "Saturday";
+      } else if (weekDay === 6) {
+        day = "Sunday";
+      }
+      map.set(day, numberOfCrime);
+    }
+    const options = {
+      title: "Distribution of crime by weekdays",
+      colors: ["DarkSlateBlue", "BlueViolet", "Crimson", "DarkCyan", "DeepPink", "ForestGreen", "DarkOrange"]
+    };
+    const chartData = [["ID"], [""]];
+    for (const [key, value] of map.entries()) {
+      chartData[0].push(key);
+      chartData[1].push(value);
+    }
+    const table = new google.visualization.arrayToDataTable(chartData);
+    chart.draw(table, options);
   }
 }
 </script>
